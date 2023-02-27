e975ef7053098cc1b69430103c02930defa778c5
Zeno Darani <zeno.darani@student.supsi.ch>
2023-02-27 21:49:06 +0100

    Completed Assignment 3

--- problemset_1/main.py
+++ problemset_1/main.py
@@ -1,7 +1,7 @@
 # Extracts a collection of birth dates from the user and determines
 # if each individual is at least 21 years old.
 from date import Date
-from copy import copy
+from copy import deepcopy
 
 def main():
     date1 = inputDate('Insert a date (month.day.year): ')
@@ -15,7 +15,7 @@ def main():
     print(f'Is year of {date1.asGregorian()} a leap year? {date1.isLeapYear()}')
     print(f'Days between {date1.asGregorian()} and {date2.asGregorian()}: {date1.numDays(date2)}')
     days = int(input(f'Insert a number of days to add at {date2.asGregorian()}: '))
-    oldDate2 = copy(date2)
+    oldDate2 = deepcopy(date2)
     date2.advanceBy(days)
     print(f'{oldDate2.asGregorian()} + {days} = {date2.asGregorian()}')
 
@@ -27,6 +27,9 @@ def main():
     print(f'Is {date1.asGregorian()} an equinox? {date1.isEquinox()}')
     print(f'Is {date1.asGregorian()} a solstice? {date1.isSolstice()}')
 
+    # Assignment 3
+    print('\n----Assignment 3----\n')
+    printCalendar(date1)
 
 # Ask for a date and returns it.
 def inputDate(msg):
@@ -36,6 +39,37 @@ def inputDate(msg):
     day = int(day)
     year = int(year)
     return Date(month, day, year)
+
+# Given a date it prints the month calendar of that date
+def printCalendar(date):
+    # print month and year centered
+    title = f'{date.monthName()} {date.year()}'
+    numSpaces = (26 - len(title)) // 2
+    print(f"{' '*numSpaces}{title}\n")
+    print('Su  Mo  Tu  We  Th  Fr  Sa')
+    currentDay = Date(date.month(), 1, date.year())
+    # prints initial empty spaces
+    dayOfWeekOrder = [6, 0, 1, 2, 3, 4, 5]
+    i = 0
+    while dayOfWeekOrder[i] != currentDay.dayOfWeek():
+        print('    ', end='')
+        i += 1
+    # prints the days
+    while currentDay.month() == date.month():
+        print(currentDay.day(), end='')
+        if currentDay.day() < 10:
+            print(' ', end='')
+        if currentDay.dayOfWeek() == dayOfWeekOrder[-1]:
+            # new line
+            print()
+        else:
+            print('  ', end='')
+        currentDay.advanceBy(1)
+
+
+
+
+
 
 # Call the main routine.
 main()
