4a09cd6d3c4b11154c6b9e99e2a93018ea50eb57
Zeno Darani <zeno.darani@hotmail.com>
2023-03-24 10:26:00 +0100

    Initialized assignment 1 of problemset 5

---
+++ problemset_5_darani/assignment_1_darani/sparse_matrix.py
@@ -0,0 +1,114 @@
+from __future__ import annotations
+from email import message
+# allows typing using the class you are defining
+from typing import Optional, Tuple
+
+
+class SparseMatrix:
+    # Create a sparse matrix of
+    def __init__(self, rows: int, cols: int):
+        if rows <= 0:
+            raise SparseMatrixException("Rows must be > 0!")
+        elif cols <= 0:
+            raise SparseMatrixException("Columns must be > 0!")
+
+        self._rows = rows
+        self._cols = cols
+        self._elements = list()  # a dynamic list is needed
+
+    # Return the number of rows
+    def rows(self) -> int:
+        return self._rows
+
+    # Return the number of columns in
+    def cols(self) -> int:
+        return self._cols
+
+    # Helper method used to find a specific matrix element (row,col) in the
+    # list of non-zero entries. None is returned if the element is not found.
+    def _find_position(self, row: int, col: int) -> Optional[int]:
+        for i, e in enumerate(self._elements):
+            if row == e.row and col == e.col:
+                return i  # return the index of the element if found.
+        return None  # return None when the element is zero.
+
+    # Set the value of element (i,j) to the value s: x[i,j] = s
+    def __setitem__(self, position: Tuple[int, int], value: float):
+
+        # find the element
+        index = self._find_position(position[0], position[1])
+
+        # if the element is NOT found in the list.
+        if index is None:
+            # zero is considered an empty element
+            if value == 0:
+                return
+
+            element = _MatrixElement(position[0], position[1], value)
+            self._elements.append(element)
+            return
+        else:  # the element is found in the list!
+            if value == 0:
+                self._elements.pop(index)
+                return
+
+            self._elements[index].value = value
+
+    def __str__(self):
+        string_repr = ""
+        for i in range(self._rows):
+            for j in range(self._cols):
+                string_repr += f"{self[i, j]}\t"
+            string_repr += "\n"
+        return string_repr
+
+    def __repr__(self) -> str:
+        return self.__str__()
+
+    # *** ASSIGNMENT 1.2 ****************************************************************************************
+    # Return the value of element (i,j) of the matrix: x[i,j]
+    def __getitem__(self, coordinates: Tuple[int, int]) -> float:
+        ... # TODO implement
+
+    def add(self, o: SparseMatrix) -> SparseMatrix:
+        ... # TODO implement
+
+    def subtract(self, o: SparseMatrix) -> SparseMatrix:
+        ... # TODO implement
+
+    def multiply(self, o: SparseMatrix) -> SparseMatrix:
+        ... # TODO implement
+
+    def transpose(self) -> SparseMatrix:
+        ... # TODO implement
+
+    # *** ASSIGNMENT 1.3 ****************************************************************************************
+    def __add__(self, o: SparseMatrix):
+        ... # TODO implement
+
+    def __mul__(self, o: SparseMatrix):
+        ... # TODO implement
+
+    def __sub__(self, o: SparseMatrix):
+        ... # TODO implement
+
+
+# Storage class for holding the non-zero matrix elements.
+class _MatrixElement:
+    def __init__(self, row, col, value):
+        self.row = row
+        self.col = col
+        self.value = value
+
+
+# simple custom Exception to manage errors
+class SparseMatrixException(Exception):
+    def __init__(self, message: str) -> None:
+        super().__init__()
+        self._message = message
+
+    def __str__(self) -> str:
+        return self._message
+    
+    def __repr__(self) -> str:
+        return self._message
