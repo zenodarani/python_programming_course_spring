971bc7848b98ab4cbc83297c58edf23bb3405dda
Zeno Darani <zeno.darani@hotmail.com>
2023-03-24 11:16:40 +0100

    Done method add

--- problemset_5_darani/assignment_1_darani/main.py
+++ problemset_5_darani/assignment_1_darani/main.py
@@ -6,3 +6,13 @@ if __name__ == "__main__":
     sparse_matrix_A[1, 2] = 12.3
     print(sparse_matrix_A)
     print(f"Vale at [2, 3] = {sparse_matrix_A[2, 3]}")
+    print(f"Vale at [1, 1] = {sparse_matrix_A[1, 1]}")
+    sparse_matrix_B = SparseMatrix(3, 4)
+    sparse_matrix_B[2, 3] = 2
+    sparse_matrix_B[1, 1] = 4
+    print(sparse_matrix_A)
+    print("-----")
+    print(sparse_matrix_B)
+    print("-----")
+    print(sparse_matrix_A.add(sparse_matrix_B))
+
