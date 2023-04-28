9fa533035139727d8e5e1016861bbca7b7bd0b1b
Zeno Darani <zeno.darani@hotmail.com>
2023-04-28 11:40:45 +0200

    Completed assignment 3.3

--- problemset_8_darani/assignment_3_darani/parenthesis_and_braces.py
+++ problemset_8_darani/assignment_3_darani/parenthesis_and_braces.py
@@ -4,10 +4,41 @@ from lliststack import Stack
 class ParenthesisAndBracesChecker:
 
     def __init__(self):
-        ...
+        self.stk_chars = Stack()
+
+    def _check_delimiter(self, delimiter, left_delimiter, right_delimiter):
+        if delimiter == right_delimiter:
+            if self.stk_chars.peek() == left_delimiter:
+                self.stk_chars.pop()
+            else:
+                raise WrongExpectedDelimiterException(delimiter)
 
     def analyze_text(self, input_text: str):
-        ...
+        for char in input_text:
+            if char in "({[":
+                self.stk_chars.push(char)
+            self._check_delimiter(char, '(', ')')
+            self._check_delimiter(char, '[', ']')
+            self._check_delimiter(char, '{', '}')
+        if not self.stk_chars.is_empty():
+            raise UnbalancedDelimitersException(self.stk_chars)
+
+
+class WrongExpectedDelimiterException(Exception):
+    def __init__(self, delimiter):
+        super().__init__(f'There is no left delimiter corresponding "{delimiter}"')
+
+
+class UnbalancedDelimitersException(Exception):
+    def __init__(self, stack_delimiters: Stack):
+        first = True
+        while not stack_delimiters.is_empty():
+            if first:
+                msg = f"Parenthesis and braces are unbalanced, remaining: {stack_delimiters.pop()}"
+                first = False
+            else:
+                msg += f", {stack_delimiters.pop()}"
+        super().__init__(f'Parenthesis and braces are unbalanced, remaining: [, (')
 
 
 if __name__ == '__main__':
